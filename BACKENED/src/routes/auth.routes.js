1c79bf311832383b1528c5142454eb91541d20f8
Harsh Verma <hv0626266@gmail.com>
2026-02-16 22:51:40 +0530

    add authentication routes and controllers; update dev script

---
+++ BACKENED/src/routes/auth.routes.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import { login, logout, signup } from '../controllers/auth.controllers';
+const authrouter = express.Router();
+
+authrouter.post('/signup',signup);
+
+authrouter.post('login',login);
+
+authrouter.post('logout',logout);
+
+
+export default authrouter;
+
