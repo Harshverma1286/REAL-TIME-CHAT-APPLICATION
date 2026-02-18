d22197aa93bbfc979ed988f0d598d2d9f24ea0a5
Harsh Verma <hv0626266@gmail.com>
2026-02-19 00:05:03 +0530

    STARTED DOING FRONTENED PART AND HAVE ARRANGED THE FILE STRUCTURES !!!

---
+++ FRONTENED/src/Pages/allpage.js
@@ -0,0 +1,13 @@
+import Homepage from "./Homepage";
+import Loginpage from "./Loginpage";
+import Profilepage from "./Profilepage";
+import Settingpage from "./Settingpage";
+import Signuppage from "./Signuppage";
+
+export{
+    Homepage,
+    Loginpage,
+    Profilepage,
+    Settingpage,
+    Signuppage
+}
